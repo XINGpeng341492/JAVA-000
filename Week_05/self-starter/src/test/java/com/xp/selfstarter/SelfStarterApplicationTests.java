7b97c69419fedde64cab22b0a55486a71bc61fcd
xingpeng <xingpeng@vipkid.com.cn>
2020-11-19 17:20:05 +0800

    self starter pom

---
+++ Week_05/self-starter/src/test/java/com/xp/selfstarter/SelfStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xp.selfstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
