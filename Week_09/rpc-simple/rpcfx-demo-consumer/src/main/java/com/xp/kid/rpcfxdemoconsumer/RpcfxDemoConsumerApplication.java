5f028fce6bf37d5733bb9849790c58e705c51e05
xingpeng <xingpeng@vipkid.com.cn>
2020-12-19 17:55:28 +0800

    week10 homework relpace jdk proxy

--- Week_09/rpc-simple/rpcfx-demo-consumer/src/main/java/com/xp/kid/rpcfxdemoconsumer/RpcfxDemoConsumerApplication.java
+++ Week_09/rpc-simple/rpcfx-demo-consumer/src/main/java/com/xp/kid/rpcfxdemoconsumer/RpcfxDemoConsumerApplication.java
@@ -3,6 +3,7 @@ package com.xp.kid.rpcfxdemoconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.xp.kid.rpcfx.client.RpcAopfx;
 import com.xp.kid.rpcfx.client.Rpcfx;
 import com.xp.kid.rpcfx.demo.api.Order;
 import com.xp.kid.rpcfx.demo.api.OrderService;
@@ -28,11 +29,11 @@ public class RpcfxDemoConsumerApplication {
         // UserService service = new xxx();
         // service.findById
 
-        UserService userService = Rpcfx.create(UserService.class, "http://localhost:8080/");
+        UserService userService = RpcAopfx.create(UserService.class, "http://localhost:8080/");
         User user = userService.findById(1);
         System.out.println("find user id=1 from server: " + user.getName());
 
-        OrderService orderService = Rpcfx.create(OrderService.class, "http://localhost:8080/");
+        OrderService orderService = RpcAopfx.create(OrderService.class, "http://localhost:8080/");
         Order order = orderService.findOrderById(1992129);
         System.out.println(String.format("find order name=%s, amount=%f",order.getName(),order.getAmount()));
 
